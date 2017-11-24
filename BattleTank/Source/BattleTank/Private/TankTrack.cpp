c195d940e847bec264037688c5aa9741ceb75bbc
Brandon <gourlayherring@gmail.com>
2017-11-24 14:01:31 +0000

    ApplyForceAtLocation() in Action

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -12,4 +12,8 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("%f: %s throttle %f"), Time, *Name, Throttle);
 
 	// TODO clamp actual throttle value so player can't over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetAttachmentRoot());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
